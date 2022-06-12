a0cc9aa11c8f64858d18f8e940f53ca77fb0c1f6
Bartłomiej <bartekbaamb@gmail.com>
2022-06-12 12:10:00 +0200

    2

--- src/Liczenie.java
+++ src/Liczenie.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class liczenie {
+public class Liczenie
+{
+
+
+
+    double delta;
+    double a;
+    double b;
+    double c;
+    double liczDelta() {
+        delta = b*b -(4*a*c);
+        return 0;
+    }
+    double deltaPierwiastek;
+    double liczPierwiastek(){
+        deltaPierwiastek = Math.sqrt(delta);
+        return 0;
+    }
+
+
+    double x1;
+    double liczX1() {
+        x1 = (-b+deltaPierwiastek) / (2*a);
+        return 0;
+    }
+    double x2;
+    double liczX2() {
+        x2 = (-b-deltaPierwiastek) / (2*a);
+        return 0;
+    }
+
+    double d1;
+    double d2;
+    public boolean x1s(){
+        return d1 < x1 & x1 < d2;
+    }
+    public boolean x2s(){
+        return d1 < x2 & x2 < d2;
+    }
+
+
+
+
+
+
 }
+
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,61 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Liczenie liczenie = new Liczenie();
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Podaj podcz¹tek dziedziny");
+        liczenie.d1 = scan.nextDouble();
+        System.out.println("Podaj koniec dziedziny");
+        liczenie.d2 = scan.nextDouble();
+
+        System.out.println("ax2 + bx + c = 0");
+
+        System.out.println("Podaj wspó³czynnik a");
+        liczenie.a = scan.nextDouble();
+        System.out.println("Podaj wspó³czynnik b");
+        liczenie.b = scan.nextDouble();
+        System.out.println("Podaj wspó³czynnik c");
+        liczenie.c = scan.nextDouble();
+
+        liczenie.liczDelta();
+        liczenie.liczPierwiastek();
+        liczenie.liczX1();
+        liczenie.liczX2();
+
+        System.out.println("Delta = b*b - 4ac = " + liczenie.delta);
+        System.out.println("D: {"+ liczenie.d1 +" ; "+ liczenie.d2+"}");
+
+        if(liczenie.x1s()){
+
+            System.out.println("x1 = -b + pierwiastek delty / 2a " + liczenie.x1);
+
+        }else {
+            System.out.println("x1 = -b + pierwiastek delty / 2a " + liczenie.x1 + "(sprzecznoœæ)");
+        }
+
+        if(liczenie.x2s()){
+            System.out.println("x2 = -b - pierwiastek delty / 2a " + liczenie.x2);
+        }else{
+            System.out.println("x2 = -b - pierwiastek delty / 2a " + liczenie.x2 + "(sprzecznoœæ)");
+        }
+
+        if(liczenie.x1s() & liczenie.x2s()){
+            System.out.println("xe {"+liczenie.x1+" ; "+liczenie.x2+"}");
+        }
+        else if(liczenie.x1s() & !liczenie.x2s()){
+            System.out.println("xe {"+liczenie.x1+"}");
+        }
+        else if(!liczenie.x1s() & liczenie.x2s()){
+            System.out.println("xe {"+liczenie.x2+"}");
+        }
+        else {
+            System.out.println("x jest puste");
+        }
+
     }
+
 }
